8bc02b0fdc44bfecd443517ce7912b22dc78a95e
Saccharin01 <chousik01@gmail.com>
2024-05-28 20:26:09 +0900

    버그 발생(진단중)

--- public/core.js
+++ public/core.js
@@ -1,20 +1,18 @@
 let root = document.getElementById(`root`)
 let controlArray = [false, false, false, false, false]
 let inputType = [`userId`, `pw`, `pwConfirm`, `email`, `phoneNum`]
-let inputName = [`userId`, `pw`, `pwConfirm`, `email`, `phoneNum`]
-let formAction = []
 
+let form = document.createElement(`form`)
+form.setAttribute(`action`, `submit`)
+form.setAttribute(`method`, `post`)
+root.appendChild(form)
 
 for(let i = 0; i < controlArray.length; i++){
-  let form = document.createElement(`form`)
-  root.appendChild(form)
-  form.setAttribute(`action`, inputType[i])
-  form.setAttribute(`method`, `post`)
   let input = document.createElement(`input`)
   form.appendChild(input);
   // input.setAttribute(`type`, 'text')
   input.setAttribute(`placeHolder`, '여기에 입력해주세요')
-  input.setAttribute(`name`, inputName[i])
+  input.setAttribute(`name`, inputType[i])
   input.setAttribute(`type`, inputType[i])
 }
 let testform = document.getElementsByTagName(`form`)
