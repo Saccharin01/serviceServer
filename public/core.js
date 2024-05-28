702b355eb5b881ef97f3267529ddc7367c5d962d
Saccharin01 <chousik01@gmail.com>
2024-05-28 15:27:23 +0900

    회원가입 폼 제작

--- public/core.js
+++ public/core.js
@@ -1,12 +1,15 @@
 let root = document.getElementById(`root`)
 let form = document.createElement(`form`)
-let input = document.createElement(`input`)
 let controlArray = [false, false, false, false, false]
 
 root.appendChild(form)
-form.appendChild(input)
 form.setAttribute(`action`, `submit`)
 form.setAttribute(`method`, `post`)
-input.setAttribute(`type`, 'text')
 input.setAttribute(`placeHolder`, '여기에 입력해주세요')
 input.setAttribute(`name`, `submitData`)
+for(let i = 0; i < controlArray.length; i++){
+  let input = document.createElement(`input`)
+  form.appendChild(input);
+  input.setAttribute(`type`, 'text')
+
+}
---
+++ public/html/core.js
@@ -0,0 +1,43 @@
+let root = document.getElementById(`root`)
+let form = document.createElement(`form`)
+let controlArray = [false, false, false, false, false]
+let inputType = [`userId`, `pw`, `pwConfirm`, `email`, `phoneNum`]
+
+root.appendChild(form)
+form.setAttribute(`action`, `submit`)
+form.setAttribute(`method`, `post`)
+
+
+for(let i = 0; i < controlArray.length; i++){
+  let input = document.createElement(`input`)
+  form.appendChild(input);
+  // input.setAttribute(`type`, 'text')
+  input.setAttribute(`placeHolder`, '여기에 입력해주세요')
+  input.setAttribute(`name`, `submitData`)
+  input.setAttribute(`type`, inputType[i])
+
+}
+
+// inputType.forEach((ele)=>{
+//   ele.setAttribute
+//   console.log(ele)
+// })
+
+// let input = document.getElementsByTagName(`input`)
+// console.log(input[1].outerHTML)
+// console.log(input.type)
+// inputType.forEach((ele)=>{
+//     input.outerHTML
+    // console.log(ele)
+
+// })
+// console.log(input)
+// input.forEach((ele)=>{
+  // ele.setAttribute(`type`, inputType)
+  //   })
+  // inputType.forEach((ele)=>{
+  //   let input = document.getElementsByTagName(`input`)
+
+
+  //   input.setAttribute('type', ele)
+  // })
