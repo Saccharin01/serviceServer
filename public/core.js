0dc736293e4cdc3dd82e88cf258bc26960799079
Saccharin01 <chousik01@gmail.com>
2024-05-28 16:31:24 +0900

    단위 로직 검증(input name type)

--- public/core.js
+++ public/core.js
@@ -1,7 +1,8 @@
 let root = document.getElementById(`root`)
 let controlArray = [false, false, false, false, false]
-let inputType = [`userId`, `pw`, `pwConfirm`, `email`, `phoneNum`]
-let inputName = []
+let inputType = [`Id`, `pw`, `pwConfirm`, `email`, `phoneNum`]
+let inputName = [`userId`, `pw`, `pwConfirm`, `email`, `phoneNum`]
+let formAction = []
 
 
 for(let i = 0; i < controlArray.length; i++){
@@ -13,7 +14,7 @@ for(let i = 0; i < controlArray.length; i++){
   form.appendChild(input);
   // input.setAttribute(`type`, 'text')
   input.setAttribute(`placeHolder`, '여기에 입력해주세요')
-  input.setAttribute(`name`, `submitData`)
+  input.setAttribute(`name`, inputName[i])
   input.setAttribute(`type`, inputType[i])
 
 }
